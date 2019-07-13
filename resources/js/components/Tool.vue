f10685564acb2253ae34fd25cdfa4603194cd7b8
Kam Lotfull <kamlotfull@gmail.com>
2019-07-13 23:31:51 +0300

    add club&service filters, events, tooltip, DateClick, EventClick & more

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
         <heading class="mb-6">Nova Fullcalendar Tool</heading>
+
+        <span>Клуб: </span>
+        <select v-model="club_selected" @input="filter">
+            <option value=""></option>
+            <option v-for="option in filters" v-bind:value="option">
+                {{ option.name }}
+            </option>
+        </select>
+
+        <span>Услуга: </span>
+        <select v-model="service_selected" @input="filter">
+            <option value=""></option>
+            <option v-for="option in club_selected.services" v-bind:value="option.id">
+                {{ option.name }}
+            </option>
+        </select>
+
         <FullCalendar
                 @dateClick="handleDateClick"
                 @eventClick="handleEventClick"
-                @eventMouseEnter="handleLeaveEnter"
-                @eventMouseLeave="handleMouseLeave"
+                @eventRender="handleEventRender"
                 ref="fullCalendar"
                 :plugins="calendarPlugins"
                 :weekends="calendarWeekends"
@@ -20,32 +36,127 @@
 </template>
 
 <script>
-
     import FullCalendar from "@fullcalendar/vue";
     import dayGridPlugin from "@fullcalendar/daygrid";
     import timeGridPlugin from "@fullcalendar/timegrid";
     import interactionPlugin from "@fullcalendar/interaction";
+    import Tooltip from 'tooltip.js';
+
+    let seance_link = seance => '/crm/resources/seances/' + seance.id;
+    let seances_request_url = '/v1/calendar/seances';
+    let filters_request_url = '/v1/calendar/filters';
+
+    let ru_translation = {
+        'first_name': 'Имя',
+        'last_name': 'Фамилия',
+        'mid_name': 'Отчество',
+        'phone': 'Номер телефона',
+        'email': 'Email',
+        'birthdate': 'Дата рождения',
+        'sex': 'Пол',
+        'passport_id': 'Номер паспорта',
+        'issue_date': 'Дата выдачи паспорта',
+        'issued_by': 'Орган выдачи паспорта',
+        'driving_license': 'Номер водительского удостоверения',
+        'driving_exp': 'Водительский стаж',
+        'driving_cat': 'Водительские категории',
+        'height': 'Рост',
+        'weight': 'Вес',
+        'clothing_size': 'Размер одежды',
+        'shoe_size': 'Размер обуви',
+    };
+
+    let prepare_client_data = client_data_json => {
+        let result = '';
+        for (let a in client_data_json) {
+            if (!!client_data_json[a])
+                result += `<p>${ru_translation[a]}: ${client_data_json[a]}</p>`;
+        }
+        return result
+    };
+
+    let calendar_event_from_seance = seance => {
+        let time = moment(seance.time, ['hh:mm:ss']);
+        let start = moment(seance.date).set({
+            hour: time.get('hour'),
+            minute: time.get('minute')
+        });
+        let end = moment(start).add({
+            minute: seance.duration
+        });
+        return {
+            title: seance.description,
+            start: start.format(),
+            end: end.format(),
+            extendedProps: {
+                description: seance.description,
+                price: seance.price,
+                duration: seance.duration,
+                service: seance.service.name,
+                client: prepare_client_data(seance.clients[0])
+            },
+            url: seance_link(seance)
+        };
+    };
 
     export default {
         components: {
             FullCalendar
         },
         methods: {
+            filter() {
+                this.set_events()
+            },
+            fetch_seances() {
+                let query = seances_request_url;
+                if (this.service_selected)
+                    query += `?service=${this.service_selected}`;
+                else if (this.club_selected)
+                    query += `?club=${this.club_selected.id}`;
+
+                return axios.get(query)
+            },
+            fetch_filters() {
+                return axios.get(filters_request_url)
+            },
+            set_events() {
+                this.fetch_seances().then(response => {
+                    let seances = response.data;
+                    if (seances) {
+                        this.calendarEvents = seances.map(calendar_event_from_seance);
+                    }
+                });
+            },
             handleDateClick(info) {
-                console.log(info);
+                if (this.service_selected) {
+                    window.open(`/crm/resources/seances/new?viaResource=services&viaResourceId=${this.service_selected}&viaRelationship=seances`, "_blank");
+                } else {
+                    window.open(`/crm/resources/seances/new`, "_blank");
+                }
             },
             handleEventClick(info) {
-                console.log(info);
+                if (info.url)
+                    window.open(info.url, "_blank");
             },
-            handleLeaveEnter(info) {
-                console.log(info);
-            },
-            handleMouseLeave(info) {
-                console.log(info);
+            handleEventRender(info) {
+                new Tooltip(info.el, {
+                    html: true,
+                    title: `<h4>${info.event.extendedProps.service}</h4>` +
+                        `<p>${info.event.extendedProps.description}</p>` +
+                        `<p>${info.event.extendedProps.price} руб.</p>` +
+                        `<p>${info.event.extendedProps.duration} минут</p>` +
+                        `<p>${info.event.extendedProps.client}</p>`,
+                    placement: 'top',
+                    trigger: 'hover',
+                    container: 'body'
+                });
             },
         },
         data() {
             return {
+                club_selected: '',
+                service_selected: '',
+                filters: {},
                 calendarPlugins: [
                     dayGridPlugin,
                     timeGridPlugin,
@@ -58,20 +169,10 @@
             };
         },
         mounted() {
-            console.log('mounted()')
-            axios.get('/horaceho/fullcalendar/events').then(response => {
-                this.events = response.data
-                if (this.events) {
-                    this.calendarEvents = []
-                    this.events.forEach((event) => {
-                        this.calendarEvents.push({
-                            title: event.title,
-                            start: event.start_date,
-                            end: event.end_date
-                        })
-                    })
-                }
+            this.fetch_filters().then(response => {
+                this.filters = response.data;
             });
+            this.set_events();
         },
     }
 
@@ -81,4 +182,14 @@
     @import "~@fullcalendar/core/main.css";
     @import "~@fullcalendar/daygrid/main.css";
     @import "~@fullcalendar/timegrid/main.css";
+
+    .tooltip {
+        z-index: 1;
+        width: 250px;
+        color: #252d37;
+        background-color: #ffffff;
+        border-radius: 3px;
+        border: 1px solid #3788d8;
+        padding: 5px;
+    }
 </style>
