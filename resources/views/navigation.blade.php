b7f3e041e23708eba454915e7fbc38ff3686a886
Kam Lotfull <kamlotfull@gmail.com>
2019-07-30 11:14:59 +0300

    update navigation blade icon

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,8 +1,17 @@
-<router-link tag="h3" :to="{name: 'calendar'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
-    <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <defs><style>.cls-1{fill:#1869c8;}</style></defs><g id="Christmas_day" data-name="Christmas day"><path class="cls-1" d="M12.27,2.15H11.9v-1a1,1,0,1,0-2,0v1H6.05v-1a1,1,0,1,0-1.95,0v1H3.73A2.56,2.56,0,0,0,1.18,4.71v.37H14.82V4.71A2.56,2.56,0,0,0,12.27,2.15ZM5.08,4a1,1,0,0,1-1-1,1.37,1.37,0,0,1,0-.19A1,1,0,0,0,6,2.85,1.37,1.37,0,0,1,6,3,1,1,0,0,1,5.08,4Zm5.86,0a1,1,0,0,1-1-1,.65.65,0,0,1,0-.19,1,1,0,0,0,.94.64,1,1,0,0,0,1-.64,1.37,1.37,0,0,1,0,.19A1,1,0,0,1,10.94,4Z"/><path class="cls-1" d="M3.61,6.05H1.18v7.19A2.56,2.56,0,0,0,3.73,15.8h8.54a2.56,2.56,0,0,0,2.55-2.56V6.05H3.61Zm-1,7.09V7.43H13.36v5.71a1.28,1.28,0,0,1-1.28,1.28H3.92A1.28,1.28,0,0,1,2.64,13.14Z"/></g></g>
-    </svg>
-    <span class="sidebar-label">
-        Календарь
-    </span>
+{{--<li class="leading-tight mb-4 text-base">--}}
+<router-link :to="{ name: 'calendar' }" class="text-white text-justify no-underline dim">
+    @if(File::exists('icons/calendar.svg'))
+        {!! File::get('icons/calendar.svg') !!}
+    @else
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="1" y="3" width="16" height="16" rx="2" stroke="white" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round"/>
+            <path d="M12.5 1V5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M5.5 1V5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M1 8.5H17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    @endif
+    <span style="margin-left: 5px;"></span>
+    {{ 'Календарь' }}
 </router-link>
+{{--</li>--}}
